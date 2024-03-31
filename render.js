ab335697c6d3eaf00d2c2cd08292fb393a62fde2
Andrey <andreybespaliy69@gmail.com>
2024-03-31 21:02:27 +0300

    Added a new functionality in basket

---
+++ render.js
@@ -0,0 +1,26 @@
+export function renderCards(data, selector) {
+    const element = document.querySelector(selector);
+
+    for (let key of data) {
+        const {id, title, price, image} = key;
+
+        const cardHTML = `
+            <div class="app__card" data-productid="${id}">
+                <div class="app__card-image-wrapper">
+                    <a href="#!"><img src="${image}" alt="" class="app__card-image"></a>
+                </div>
+
+                <div class="app__card-about">
+                    <a href="#!" class="app__card-title">${title}</a>
+                    <p class="app__card-price">${price}</p>
+                </div>
+                
+
+                <button class="addToCart-btn">Добавить в корзину</button>
+            </div>
+        `
+
+
+        element.insertAdjacentHTML('beforeend', cardHTML)
+    }
+}
--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 
 import { 
     getBasketFromLocalStorage,
-    setCardToLocalStorage
-} from "./localStorage.js";
+    setCardToLocalStorage,
+    renderBasketLength
+} from './localStorage.js';
+
+import {renderCards} from './render.js'
 
 async function getPhotos(url) {
     try {
@@ -20,36 +23,10 @@ async function getPhotos(url) {
 }
 
 getPhotos('https://fakestoreapi.com/products')
-    .then(res => renderCards(res, '.app')) 
+    .then(res => renderCards(res, '.app'))
+    .then(() => renderBasketLength()) 
     .catch(err => console.error('Something went wrong', err)); 
 
- 
-function renderCards(data, selector) {
-    const element = document.querySelector(selector);
-
-    for (let key of data) {
-        const {id, title, price, image} = key;
-
-        const cardHTML = `
-            <div class="app__card" data-productid="${id}">
-                <div class="app__card-image-wrapper">
-                    <a href="#!"><img src="${image}" alt="" class="app__card-image"></a>
-                </div>
-
-                <div class="app__card-about">
-                    <a href="#!" class="app__card-title">${title}</a>
-                    <p class="app__card-price">${price}</p>
-                </div>
-                
-
-                <button class="addToCart-btn">Добавить в корзину</button>
-            </div>
-        `
-
-
-        element.insertAdjacentHTML('beforeend', cardHTML)
-    }
-}
 
 const productList = document.querySelector('.app')
 productList.addEventListener('click', addToBasket)
@@ -71,15 +48,14 @@ function addToBasket(e) {
         basket = []
     }
  
-    if (!basket.includes(id)) {
-        basket.push(id)
-    } else {
+   if (basket.includes(id)) {
         const index = basket.indexOf(id)
         basket.splice(index, 1)
-    }
+   } else {
+        basket.push(id)
+   }
 
     setCardToLocalStorage(basket) 
     
-    console.log(basket)
 }
 
